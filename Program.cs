29081a4dd5a8f9fca90910d79c6cf18b51c1677f
skakizaki <shohachi.kakizaki@gmail.com>
2024-02-11 08:08:41 +0900

    使用中のファイルをイベントログに出力。

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Automation;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
 
 
 namespace beartrap {
     internal static class Program {
+        private static string windowName;
+        private static string buttonName;
+        private static string EVENTLEG_SOURCE = "beartrap";
+        private static AutomationElement DialogElement = null;
+
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
@@ -26,16 +33,13 @@ namespace beartrap {
                 TreeScope.Descendants,
                 HandleOpenEvent);
 
-            Console.WriteLine("Invoke event handler registered. Press Enter to exit.");
+            EventLog.WriteEntry(EVENTLEG_SOURCE, "イベントハンドラを設定しました", EventLogEntryType.Information, 100);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Form1 form = new Form1();
             Application.Run();
         }
-
-        private static string windowName;
-        private static string buttonName;
 
         static void Capture() {
             // スクリーンショットを取得するディスプレイを選択
@@ -45,13 +49,6 @@ namespace beartrap {
             string fileName = "C:\\temp\\screenshot_" + DateTime.Now.ToString("yyyy-MM-dd_HHmmss") + ".png";
 
             // スクリーンショットを取得して保存
-            CaptureScreen(screenIndex, fileName);
-
-            Console.WriteLine("スクリーンショットが保存されました: " + fileName);
-        }
-
-        static void CaptureScreen(int screenIndex, string outputPath) {
-            // スクリーンのサイズを取得
             Screen screen = Screen.AllScreens[screenIndex];
             Rectangle bounds = screen.Bounds;
 
@@ -59,33 +56,110 @@ namespace beartrap {
             using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
             using (Graphics g = Graphics.FromImage(bitmap)) {
                 g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-                bitmap.Save(outputPath);
+                bitmap.Save(fileName);
             }
+
+            EventLog.WriteEntry(EVENTLEG_SOURCE,
+                $"スクリーンショットが保存されました:{fileName}",
+                EventLogEntryType.Information, 111);
         }
 
         // UI Automation Invokeイベントハンドラ
         private static void HandleInvokeEvent(object sender, AutomationEventArgs e) {
-            Console.WriteLine("Invoke event handled.");
+            EventLog.WriteEntry(EVENTLEG_SOURCE, "ボタンが押されました", EventLogEntryType.Information, 110);
 
             // 画面キャプチャの取得
             Capture();
+
+            List<string> window_names = new List<string>();
+            // 操作中のファイルの取得
+            TreeWalker treeWalker = TreeWalker.ControlViewWalker;
+            AutomationElement mainWindow = treeWalker.GetParent(DialogElement); // main window
+
+            // MDI のクライアント領域を取得
+            AutomationElement workPane =
+                mainWindow.FindFirst(TreeScope.Children,
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Pane)
+                    );
+
+            // MDI 内のウインドウを取得
+            Condition condition = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window);
+            AutomationElementCollection workWindows = workPane.FindAll(TreeScope.Children, condition);
+            foreach (AutomationElement workWindow in workWindows) {
+                string name = workWindow.GetCurrentPropertyValue(AutomationElement.NameProperty) as string;
+                // ウインドウのタイトルが作業中のファイル名
+                window_names.Add(name);
+            }
+
+            // テーブル内テキストの取得
+            List<string> table_text_lines = new List<string>();
+
+            // グリッドコントロールを取得
+            var gridControl = DialogElement.FindFirst(TreeScope.Children,
+                new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.DataGrid));
+
+            var tablePattern = gridControl.GetCurrentPattern(TablePattern.Pattern) as TablePattern;
+            if (tablePattern != null) {
+                // グリッド内の行と列を反復処理し、各セルのテキストを取得して表示
+                List<string> lineItems = new List<string>();
+                for (int row = 0; row < tablePattern.Current.RowCount; row++) {
+                    lineItems.Clear();
+                    for (int column = 0; column < tablePattern.Current.ColumnCount; column++) {
+                        var cellElement = tablePattern.GetItem(row, column);
+                        var text = cellElement.Current.Name;
+                        lineItems.Add(text);
+                        // Console.WriteLine($"Row {row}, Column {column}: {text}");
+                    }
+                    table_text_lines.Add(String.Join(",", lineItems));
+                }
+            } else {
+                Console.WriteLine("Table Control Pattern not supported.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in window_names) { sb.AppendLine(item); }
+            foreach (var item in table_text_lines) { sb.AppendLine(item.ToString()); }
+            var data = sb.ToString();
+            EventLog.WriteEntry(EVENTLEG_SOURCE,
+                $"データの状況は以下の通りです\n{data}",
+                EventLogEntryType.Information, 111);
         }
+
 
         private static void HandleOpenEvent(object sender, AutomationEventArgs e) {
             AutomationElement sourceElement = sender as AutomationElement;
             string window_title = sourceElement.GetCurrentPropertyValue(AutomationElement.NameProperty) as string;
-            Console.WriteLine("Open event handled.{0}", window_title);
+
+            Console.WriteLine($"ウインドウが表示されました:{window_title}");
+
             if (window_title == windowName) {
+                EventLog.WriteEntry(EVENTLEG_SOURCE,
+                    $"ウインドウが表示されました:{window_title}",
+                    EventLogEntryType.Information, 110);
+
+                DialogElement = sourceElement;
+
                 // ウインドウ内の「CD作成」ボタンを取得
                 Condition condition = new PropertyCondition(AutomationElement.NameProperty, buttonName);
                 AutomationElement cdBurnButton = sourceElement.FindFirst(TreeScope.Descendants, condition);
 
-                // Invokeイベントハンドラを登録
-                Automation.AddAutomationEventHandler(
-                    InvokePattern.InvokedEvent,
-                    cdBurnButton,
-                    TreeScope.Element,
-                    HandleInvokeEvent);
+                if (cdBurnButton != null) {
+                    // Invokeイベントハンドラを登録
+                    Automation.AddAutomationEventHandler(
+                        InvokePattern.InvokedEvent,
+                        cdBurnButton,
+                        TreeScope.Element,
+                        HandleInvokeEvent);
+
+                    EventLog.WriteEntry(EVENTLEG_SOURCE,
+                        $"ハンドラを登録しました:{buttonName}",
+                        EventLogEntryType.Information, 110);
+                }
+                else {
+                    EventLog.WriteEntry(EVENTLEG_SOURCE,
+                        $"NameProperty が {buttonName} であるコントロールが見つかりませんでした",
+                        EventLogEntryType.Warning, 110);
+                }
             }
         }
     }
---
+++ README.md
@@ -0,0 +1,7 @@
+### インストール
+
+イベントログのログソース追加のために、一度だけ管理者権限で以下のコマンドを実行してください
+
+```
+eventcreate /L APplication /SO beartrap /ID 100 /T INFORMATION /D "install"
+```
